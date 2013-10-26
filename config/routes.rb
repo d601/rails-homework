b90190c3807caa8ead3126ac560ca5d156bc388c
d601 <shmeep@gmail.com>
2013-10-26 12:53:30 -0700

    Add calendar route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Homework1::Application.routes.draw do
   root to: "pages#home"
 
   get 'pages/project_2' => 'pages#project_2'
+  get '/calendar' => 'pages#calendar'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
