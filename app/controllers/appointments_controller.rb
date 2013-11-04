3307672142c9d310aead7265085618f85513453c
d601 <shmeep@gmail.com>
2013-11-03 20:45:32 -0800

    Start working on ajax response - incomplete

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,7 +1,43 @@
 class AppointmentsController < ApplicationController
   def index
+    @appointments = Appointment.all
   end
   
   def new
   end
+
+  def create
+    # Time should probably be sanitized - or does rails do that automatically?
+    post_data = params[:appointment]
+
+    @appointment = Appointment.new
+    @appointment.description = post_data[:description]
+
+    begin
+      @appointment.time = DateTime.new(Integer(post_data[:year]),
+                                      Integer(post_data[:month]),
+                                      Integer(post_data[:day]),
+                                      Integer(post_data[:hour]),
+                                      Integer(post_data[:minute]))
+    rescue ArgumentError
+      render json: "error"
+      return
+    end
+
+    if @appointment.save
+      render json: @appointment, status: :created, location: @appointment
+    else
+      render json: @appointment.errors, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    render text: "incomplete"
+  end
+
+  private
+    def appointment_params
+      # params.permit(:year, :month, :day, :hour, :minute, :description)
+      # params.require(:appointment).permit(:time, :description)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Homework1::Application.routes.draw do
   get 'pages/project_2' => 'pages#project_2'
   get '/calendar' => 'pages#calendar'
 
-  resources :appointments, only: [:index, :new, :create]
+  resources :appointments, only: [:index, :new, :create, :show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
