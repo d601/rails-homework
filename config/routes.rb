3b8372ac3778385cdd6476da0b11656a08d618f9
Paul Sladek <pwbsladekcw@gmail.com>
2013-11-06 18:11:53 -0800

    added apointments resource

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Homework1::Application.routes.draw do
   get 'pages/project_2' => 'pages#project_2'
   get '/calendar' => 'pages#calendar'
 
+  resources :appointments
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
